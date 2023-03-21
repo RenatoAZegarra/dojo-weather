51e70918be1de7d92a9a1a1303c0f5d70b7a4c3c
RenatoAZegarra <Zegarrarenatoa@gmail.com>
2023-03-21 15:18:22 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function deleteCookie(element){
+  element.remove();
+}
+
+function changetoF(element,element2,element3,element4,element5,element6,element7,element8){
+  element.innerText= Math.floor((element.innerText*1.8)+32);
+  element2.innerText= Math.floor((element2.innerText*1.8)+32);
+  element3.innerText= Math.floor((element3.innerText*1.8)+32);
+  element4.innerText= Math.floor((element4.innerText*1.8)+32);
+  element5.innerText= Math.floor((element5.innerText*1.8)+32);
+  element6.innerText= Math.floor((element6.innerText*1.8)+32);
+  element7.innerText= Math.floor((element7.innerText*1.8)+32);
+  element8.innerText= Math.floor((element8.innerText*1.8)+32);
+}
